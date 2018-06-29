295f8b57bc7ff6849b8943ee4d17cc4f25612ff5
liuqituo <1815465998@qq.com>
2018-06-29 21:31:58 +0800

    change

---
+++ routes/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql:{
+        host: '10.10.4.162',
+        user: 'root',
+        password: 'xll',
+        port: '3306',
+        database: 'plugin'
+    }
+}
--- routes/test.js
+++ routes/test.js
@@ -4,16 +4,12 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var connect = mysql.createConnection({
-    host     : '10.10.4.162',
-    user     : 'root',
-    password : 'xll',
-    port: '3306',
-    database: 'plugin'
-});
+var module = require('db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var sql = 'select * from js';
+    var connect = mysql.createConnection(module.mysql);
+    connect.connect();
     connect.query(sql,function (err, result) {
         res.send(result);
     })
