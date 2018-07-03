cb0314fccdb8c746c901fdb6cad0a5e5bd1224f5
love <534747442@qq.com>
2018-07-03 16:17:10 +0800

    拷贝文件模块

--- routes/dele_file.js
+++ routes/dele_file.js
@@ -4,12 +4,17 @@ var dele = {
      * @param path
      */
     dele: function(path) {
+      var me=this;
         var fs = require('fs');
-           var files = fs.readdirSync(path);
-           files.forEach(function(file){
-           	fs.unlinkSync(path+'/'+file);
-           });
-           fs.rmdirSync(path);
+        var files = fs.readdirSync(path);
+        files.forEach(function(file) {
+            if (file.indexOf('.') !== -1)
+                {fs.unlinkSync(path + '/' + file);}
+              else{
+                me.dele(path+"/"+file);
+              }
+        });
+        fs.rmdirSync(path);
     }
 };
-module.exports=dele;
+module.exports = dele;
