ac2803752fb2823a93ee9a9589b062ab4ec0c822
love <534747442@qq.com>
2018-07-03 10:11:56 +0800

    拷贝文件模块

--- routes/copy_file.js
+++ routes/copy_file.js
@@ -1,15 +1,22 @@
+//由于各种原因可能导致压缩的文件夹存在故要在新建文件夹之前删掉
+//加载删除模块
+var dele=require('./dele_file');
 var copy = {
     //由于主模块要使用 故全部采用了同步读取和写入的方式
     copyFile: function(from, to) {
-        var tem=['/index.js','/index.css',"/index.js",'/nodom-full.js','/index.html']；
+        var tem=['/index.js','/index.css',"/index.js",'/nodom-full.js','/index.html'];
         var fs = require("fs");
-        var buffer=new Buffer(65535);
         var des_path=to+'plugin';
+        if(fs.existsSync(des_path)){
+            dele.dele(des_path);
+        }
         fs.mkdirSync(des_path);
-        tem.forEach(function(item,index,arr){
+        tem.forEach(function(item){
             var str=fs.readFileSync(from+item,'utf-8');
             fs.writeFileSync(des_path+item,str);
         });
+        //由于太慢了所以返回一个值
+        return true;
     }
-}
+};
 module.exports = copy;
