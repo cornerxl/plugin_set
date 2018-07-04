7a3e2c9d52c48699873a83050ae59de676a02e77
love <534747442@qq.com>
2018-07-04 11:26:41 +0800

    配置模块 支持多人同时配置

--- routes/config_file.js
+++ routes/config_file.js
@@ -3,29 +3,26 @@ var config = {
      *
      * @param obj
      * @param path
+     * @param class_str
      */
-    init: function(obj, path) {
-        var fs = require("fs");
-        var str_old = fs.readFileSync(path + '/css/page.css', 'utf-8');
+    init: function(obj, path, class_str) {
         var me = this;
+        var fs = require("fs");
+        var str_old = fs.readFileSync(path + '/css/slideImg.css', 'utf-8');
         var tem = Object.getOwnPropertyNames(obj);
         var config = [];
         var count = 0;
         for (var k in obj) {
-            if (count !== 0) {
-                config.push(';' + tem[count] + ':' + obj[k]);
-            } else {
-                config.push(tem[count] + ':' + obj[k]);
-            }
+            config.push(tem[count] + ':' + obj[k] + ';');
             count++;
         }
-        var str_start = str_old + '.a {';
+        var str_start = str_old + class_str + '{';
         var str_end = '}';
         config.forEach(function(i) {
             str_start += i;
         });
         str_start += str_end;
-        fs.writeFileSync(path + '/css/page.css', str_start, 'utf-8');
+        fs.writeFileSync(path + '/css/slideImg.css', str_start, 'utf-8');
     }
 };
 module.exports = config;
