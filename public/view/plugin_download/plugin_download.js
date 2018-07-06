a5f7df6de066470032ebf91de18d4535638e49e5
love <534747442@qq.com>
2018-07-06 16:02:23 +0800

    下载配置页面

--- public/view/plugin_download/plugin_download.js
+++ public/view/plugin_download/plugin_download.js
@@ -4,6 +4,31 @@
 (function () {
     DD.createModule({
         name: 'm_plugin_download',
-        templateUrl: HTMLURL + '/plugin_download/plugin_download.html'
+        el:'.el-downlist',
+        templateUrl: HTMLURL + '/plugin_download/plugin_download.html',
+        data:{
+        	public_path:"/plugin_set/public/plugin/plugin_down/",
+        	src:''
+        },
+        methods:{
+        	download:function(){
+        		var me=this;
+        		var params={
+        			  background:"red",
+        			  border:1+'px'+" "+"solid"+" "+"black",
+        		};
+        		params["font-size"]="1px";
+        		DD.request({
+        			params:params,
+        			url:"http://localhost:3000/test?",
+        			successFunc:function(r){
+        				me.data.src=me.data.public_path+r.split("/")[r.split('/').length-1];
+        			}
+        		});
+        	},
+        	close:function(){
+        		me.data.show=false;
+        	}
+        }
     })
 }())
