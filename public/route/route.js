ab40f404628d7f1905fe1c8cdf69ee2eafc48bac
Cheng <924093945@qq.com>
2018-07-06 22:42:41 +0800

    初步完成展示页面

--- public/route/route.js
+++ public/route/route.js
@@ -6,18 +6,30 @@
         {
             path: "/route",
             module: 'm_index',
-            routes:[
+            routes: [
                 {
                     path: '/home',
                     module: 'm_home'
-                },{
+                }, {
                     path: '/plugin_list',
-                    module: 'm_plugin_list'
-                },{
+                    module: 'm_plugin_list',
+                    routes: [{
+                        path: '/A',
+                        module: 'm_home'
+                    }, {
+                        path: '/B',
+                        module: 'm_home'
+                    }, {
+                        path: '/C',
+                        module: 'm_home'
+                    }]
+                }, {
                     path: '/plugin_download',
                     module: 'm_plugin_download'
-                }
+                },
             ]
         }
     ])
+    DD.Router.switch.style='slide';
+    DD.Router.switch.time=0.5;
 }())
--- public/view/plugin_list/plugin_list.js
+++ public/view/plugin_list/plugin_list.js
@@ -5,6 +5,30 @@
 (function () {
     DD.createModule({
         name: 'm_plugin_list',
-        templateUrl: HTMLURL + '/plugin_list/plugin_list.html'
+        el:'.el-plugin-list',
+        templateUrl: HTMLURL + '/plugin_list/plugin_list.html',
+        data:{
+            plugin_list:[
+                {
+                    name:"分页插件",
+                    path:"/route/plugin_list/A",
+                    active:true
+                },{
+                    name:"Switcher插件",
+                    path:"/route/plugin_list/B",
+                    active:false
+                },{
+                    name:"日历插件",
+                    path:"/route/plugin_list/C",
+                    active:false
+                }
+            ]
+        },
+        onBeforeFirstRender:function () {
+            var me = this;
+            // me.data.plugin_list.forEach(function (item) {
+            //     item.active=false;
+            // })
+        }
     })
 }())
